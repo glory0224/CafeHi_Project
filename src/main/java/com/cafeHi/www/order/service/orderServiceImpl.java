f32dcedf8dfc3cdb381d4bd36297f76e22e5e51d
twozeropang <pma5758@naver.com>
2023-02-05 19:41:58 +0900

    orderService code 추가

--- src/main/java/com/cafeHi/www/order/service/orderServiceImpl.java
+++ src/main/java/com/cafeHi/www/order/service/orderServiceImpl.java
@@ -2,7 +2,31 @@ package com.cafeHi.www.order.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class orderServiceImpl {
+import com.cafeHi.www.order.dao.orderDAO;
+import com.cafeHi.www.order.dto.orderMenuDTO;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class orderServiceImpl implements orderService{
+	
+	private final orderDAO orderDAO;
+	
+	
+	@Override
+	public void insertOrderMenu(orderMenuDTO orderMenu) {
+		orderDAO.insertOrderMenu(orderMenu);
+	}
+
+	@Override
+	public void updateOrderMenu(orderMenuDTO orderMenu) {
+		orderDAO.updateOrderMenu(orderMenu);
+	}
+
+	@Override
+	public void deleteOrderMenu(orderMenuDTO orderMenu) {
+		orderDAO.deleteOrderMenu(orderMenu);
+	}
+	
 }
