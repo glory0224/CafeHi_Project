4dbee5c972e5801cde12d31705d4e36294973590
twozeropang <pma5758@naver.com>
2023-02-18 21:15:00 +0900

    membership Point add 구상

--- src/main/java/com/cafeHi/www/membership/service/MembershipImpl.java
+++ src/main/java/com/cafeHi/www/membership/service/MembershipImpl.java
@@ -37,6 +37,35 @@ public class MembershipImpl implements MembershipService{
 		
 		membershipDAO.plusMembershipPoint(point);
 	}
+	
+	
+	//멤버쉽 계산 메소드
+	
+	public long CalMembershipPoint() {
+		
+		// 비지니스 로직
+		
+		// 멤버쉽 등급 
+		
+		// VIP = 주문한 메뉴 가격의 20%를 point 적립
+		
+		// Gold = 주문한 메뉴 가격의 15%를 point 적립
+		
+		// Silver = 주문한 메뉴 가격의 10%를 point 적립
+		
+		// Standard = 주문한 메뉴 가격의 5%를 point 적립
+		
+		
+		// 필요한 매개변수
+		
+		// member_code -> order 정보 -> orderMenu 정보 -> 메뉴 총 금액 * (등급별 % 적용) ? 
+		
+		
+		long num = 100L;
+		
+		return num;
+		
+	}
 
 	
 
