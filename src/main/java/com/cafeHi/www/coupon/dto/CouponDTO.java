b2ebddd703b01a2604f4dcb80513f7e7d0be252a
twozeropang <pma5758@naver.com>
2023-02-25 23:33:53 +0900

    CouponDTO code 작성

--- src/main/java/com/cafeHi/www/coupon/dto/CouponDTO.java
+++ src/main/java/com/cafeHi/www/coupon/dto/CouponDTO.java
@@ -1,5 +1,15 @@
 package com.cafeHi.www.coupon.dto;
 
-public class CouponDTO {
+import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
+public class CouponDTO {
+	private int coupon_code;
+	private String coupon_name;
+	private String coupon_content;
+	private int coupon_price;
+	private LocalDateTime coupon_start;
+	private LocalDateTime coupon_end;
 }
