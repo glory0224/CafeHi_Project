1260085203421750dcca6e83567a70ad5a64aae1
twozeropang <pma5758@naver.com>
2023-03-10 19:56:53 +0900

    페이징 기능을 위한 QnA 더미 데이터 삽입 테스트 코드 작성

---
+++ src/test/java/com/cafeHi/www/board/qna/QnATest.java
@@ -0,0 +1,33 @@
+package com.cafeHi.www.board.qna;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cafeHi.www.board.qna.dao.QnADAO;
+import com.cafeHi.www.board.qna.dto.QnADTO;
+
+import lombok.Setter;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/security-context.xml"
+})
+public class QnATest {
+	
+	@Setter(onMethod= @__({@Autowired}))
+	private QnADAO qnADAO;
+	
+	@Test
+	public void insertQnA() {
+		for (int i = 1; i < 51; i++) {
+			QnADTO qna = new QnADTO("test User title" + i, "none", "test User content" + i, LocalDateTime.now(), LocalDateTime.now(), 0, "none", "none", "¾øÀ½", i);
+			qnADAO.insertQnA(qna);
+		}
+	}
+}
